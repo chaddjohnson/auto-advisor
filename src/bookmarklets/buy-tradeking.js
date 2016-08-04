cef7abe05cb7186e57b59d29cca52139015ea399
Chad Johnson <chad.d.johnson@gmail.com>
2016-08-04 13:52:41 -0500

    Improved TradeKing Buy bookmarklet.

--- src/bookmarklets/buy-tradeking.js
+++ src/bookmarklets/buy-tradeking.js
@@ -1,11 +1,17 @@
-;(function() {
-    var symbol, commission, balance, askPrice, shares;
+(function() {
+    var symbol, commission, buyingPower, amount, askPrice, shares;
 
     commission = 4.95;
     symbol = jQuery('#symbol').val() || prompt('Symbol');
 
-    // Find the balance.
-    balance = parseFloat(jQuery('#balanceTable tbody tr:nth-child(4) .BalDataRowCash').text().replace(/[\$\,]/g, ''));
+    // Find the day trading buying power.
+    buyingPower = parseFloat(jQuery('#balanceTable tbody tr:nth-child(1) .BalDataRowCash').text().replace(/[\$\,]/g, ''));
+
+    // Ask for the amount to trade, defaulting to the buying power.
+    amount = window.bookmarkletAmount || prompt('Amount', buyingPower).replace(/[\$\,]/g, '');
+
+    // Track the amount globally.
+    window.bookmarkletAmount = amount;
 
     // Select "Buy".
     jQuery('#transaction_1').click();
@@ -24,8 +30,8 @@
         // Find the ask price.
         askPrice = parseFloat(jQuery('#quotePanelAsk0').text());
 
-        // Calculate the number of shares that can be purchased using the balance.
-        shares = Math.floor((balance - commission) / askPrice);
+        // Calculate the number of shares that can be purchased using the amount.
+        shares = Math.floor((amount - commission) / askPrice);
 
         // Enter the number of shares.
         jQuery('#amount').val(shares);
