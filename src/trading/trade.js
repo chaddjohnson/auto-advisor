a0cb7e51ba77cbcba077ac1e86339a36208a38d4
Chad Johnson <chad.d.johnson@gmail.com>
2016-08-17 03:40:48 -0500

    Updated trading client to do up averaging.

--- src/trading/trade.js
+++ src/trading/trade.js
@@ -22,10 +22,6 @@ var quoteDatetime = '';
 var holdingQty = 0;
 var holdingCostBasis = 0;
 var cash = 0;
-var quotes = [];
-var buyHistory = [];
-var sequentialBuyDays = 0;
-var sequentialIncreaseDays = 0;
 var activityOccurred = false;
 
 // Set up the trading client.
@@ -38,7 +34,7 @@ var smsClient = new (require('../../lib/smsClient'))(config.sms);
 var tasks = [];
 
 // Delay between buy/sell and balance lookup.
-var delay = process.env.NODE_ENV === 'production' ? 60 * 1000 : 0;
+var delay = process.env.NODE_ENV === 'production' ? 30 * 1000 : 0;
 
 function formatDollars(number) {
     return '$' + number.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
@@ -84,8 +80,6 @@ tasks.push(function(taskCallback) {
             daysHeld = 0;
         }
 
-        buyHistory = data || [];
-
         taskCallback();
     }).catch(function(error) {
         taskCallback(error);
@@ -121,19 +115,16 @@ tasks.push(function(taskCallback) {
     // Calculate the average cost basis of the holdings.
     var averageHoldingCostBasis = holdingCostBasis / holdingQty;
 
-    // Calculate the target sell price.
-    var targetSellPrice = averageHoldingCostBasis * (1 + (config.sellTriggerProfitPercentage / 100));
+    // Determine whether the stop loss threshold has been reached.
+    var stopLossThresholdReached = price <= averageHoldingCostBasis * (1 - (config.stopLossThreshold / 100));
 
-    // Determine whether the target price has been reached.
-    var targetPriceReached = price >= targetSellPrice;
-
-    // Determine whether the holding has been held too long but the break even price has been reached.
-    var heldTooLongAndBreakEvenReached = daysHeld >= config.maxDaysHeld && price >= averageHoldingCostBasis;
+    // Determine whether the holding has been held too long.
+    var heldTooLong = daysHeld >= config.maxDaysHeld;
 
     // Track cash prior to sell so that net profit can be calculated.
     var previousCash = cash;
 
-    if (holdingQty > 0 && (targetPriceReached || heldTooLongAndBreakEvenReached)) {
+    if (holdingQty > 0 && (stopLossThresholdReached || heldTooLong)) {
         tradingClient.sell(config.symbol, holdingQty).then(function() {
             // Add a multi-second delay to let things settle.
             setTimeout(function() {
@@ -166,149 +157,20 @@ tasks.push(function(taskCallback) {
     }
 });
 
-// Download and parse stock data from Yahoo.
-tasks.push(function(taskCallback) {
-    // Do nothing if there are no positions held.
-    if (holdingQty === 0) {
-        return taskCallback();
-    }
-
-    var now = new Date();
-    var options = {
-        url: 'http://real-chart.finance.yahoo.com/table.csv?s=' + config.symbol + '&a=0&b=01&c=' + (now.getUTCFullYear() - 1) + '&d=' + now.getUTCMonth() + '&e=' + now.getUTCDate() + '&f=' + now.getUTCFullYear() + '&g=d&ignore=.csv',
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-        }
-    };
-
-    request(options, function(error, response, body) {
-        if (error) {
-            return taskCallback(error);
-        }
-
-        var lines = body.toString().split('\n');
-
-        if (lines[0] !== 'Date,Open,High,Low,Close,Volume,Adj Close') {
-            return taskCallback('Bad quote data.');
-        }
-
-        // Remove the header.
-        lines.shift();
-
-        lines.forEach(function(line, index) {
-            if (line.length === 0) {
-                return;
-            }
-
-            var lineParts = line.split(',');
-
-            quotes.push({
-                date: lineParts[0],
-                close: parseFloat(lineParts[4])
-            });
-        });
-
-        taskCallback();
-    });
-});
-
-// Count sequential increase days.
-tasks.push(function(taskCallback) {
-    // Do nothing if there are no positions held.
-    if (holdingQty === 0) {
-        return taskCallback();
-    }
-
-    var countingDone = false;
-    var previousSequentialQuote = null;
-
-    // Iterate through quotes to determine the number of sequential increase days.
-    quotes.forEach(function(quote) {
-        if (countingDone) {
-            return;
-        }
-        if (!previousSequentialQuote) {
-            previousSequentialQuote = quote;
-            return;
-        }
-
-        var percentChange = ((previousSequentialQuote.close / quote.close) - 1) * 100;
-
-        if (percentChange > 0) {
-            sequentialIncreaseDays++;
-        }
-        else {
-            countingDone = true;
-        }
-
-        previousSequentialQuote = quote;
-    });
-
-    taskCallback();
-});
-
-// Count sequential buy days.
-tasks.push(function(taskCallback) {
-    // Do nothing if there are no positions held.
-    if (holdingQty === 0) {
-        return taskCallback();
-    }
-
-    // Do nothing if there is no buy history.
-    if (!buyHistory || buyHistory.length === 0) {
-        return taskCallback();
-    }
-
-    var countingDone = false;
-    var buyQuoteIndex = -1;
-
-    // Find the quote index of the most recent buy.
-    quotes.forEach(function(quote, index) {
-        if (quote.date === buyHistory[0].date.match(/^\d{4}\-\d{2}\-\d{2}/)[0]) {
-            buyQuoteIndex = index;
-        }
-    });
-
-    buyHistory.forEach(function(historyItem, index) {
-        if (countingDone) {
-            return;
-        }
-        if (!quotes[buyQuoteIndex]) {
-            return;
-        }
-
-        if (quotes[buyQuoteIndex].date === historyItem.date.match(/^\d{4}\-\d{2}\-\d{2}/)[0]) {
-            sequentialBuyDays++;
-        }
-        else {
-            countingDone = true;
-        }
-
-        buyQuoteIndex++;
-    });
-
-    // Zero out the sequential buy days if the sequential increase days is high enough.
-    if (sequentialIncreaseDays >= 2) {
-        sequentialBuyDays = 0;
-    }
-
-    taskCallback();
-});
-
 // Buy?
 tasks.push(function(taskCallback) {
     var percentChange = ((price / previousClosePrice) - 1) * 100;
 
     // Possibly buy if the security has decreased in value.
-    if (percentChange < 0 && sequentialBuyDays < 4) {
-        let investment = baseInvestment * (percentChange / config.investmentFactor) * -1;
+    if (percentChange > 0) {
+        let investment = baseInvestment * (percentChange / config.investmentFactor);
         let qty = Math.floor(investment / price);
         let costBasis = (qty * price) + config.brokerage.commission;
 
         // Track cash prior to sell so that net profit can be calculated.
         let previousCash = cash;
 
-        if (investment > cash) {
+        if (cash - costBasis <= 0) {
             return taskCallback(config.symbol + ' dropped ' + percentChange.toFixed(2) + '% since previous close from ' + formatDollars(previousClosePrice) + ' to ' + formatDollars(price) + '. Potential investment amount exceeds balance. Consider placing a manual trade.');
         }
 
@@ -321,13 +183,8 @@ tasks.push(function(taskCallback) {
                         // Calculate the average cost basis of the holdings.
                         var averageHoldingCostBasis = data.holdingCostBasis / data.holdingQty;
 
-                        // Calculate the target sell price.
-                        var targetSellPrice = averageHoldingCostBasis * (1 + (config.sellTriggerProfitPercentage / 100));
-
-                        // If the holding has been held too long, then the target price is the break even price.
-                        if (daysHeld >= config.maxDaysHeld) {
-                            targetSellPrice = averageHoldingCostBasis;
-                        }
+                        // Calculate the stop loss price.
+                        var stopLossPrice = averageHoldingCostBasis * (1 - (config.stopLossThreshold / 100));
 
                         // Update the cash available.
                         cash = data.cash;
@@ -338,7 +195,7 @@ tasks.push(function(taskCallback) {
                         console.log(config.symbol + '\t' + 'BUY' + '\t' + quoteDatetime.match(/^\d{4}\-\d{2}\-\d{2}/)[0] + '\t' + percentChange.toFixed(2) + '%\t' + qty + '\t' + formatDollars(price) + '\t\t' + formatDollars(previousCash - cash) + ' \t\t\t' + formatDollars(cash));
 
                         // Send an SMS.
-                        smsClient.send(config.sms.toNumber, config.symbol + ' dropped ' + percentChange.toFixed(2) + '% since previous close from ' + formatDollars(previousClosePrice) + ' to ' + formatDollars(price) + '. Successfully bought ' + qty + ' shares of ' + config.symbol + ' using ' + formatDollars(previousCash - cash) + '. Target price is ' + formatDollars(targetSellPrice) + '. New balance is ' + formatDollars(cash) + '.');
+                        smsClient.send(config.sms.toNumber, config.symbol + ' increased ' + percentChange.toFixed(2) + '% since previous close from ' + formatDollars(previousClosePrice) + ' to ' + formatDollars(price) + '. Successfully bought ' + qty + ' shares of ' + config.symbol + ' using ' + formatDollars(previousCash - cash) + '. Stop loss price is ' + formatDollars(stopLossPrice) + '. New balance is ' + formatDollars(cash) + '.');
 
                         taskCallback();
                     }).catch(function(error) {
@@ -362,9 +219,7 @@ tasks.push(function(taskCallback) {
 async.series(tasks, function(error) {
     if (error) {
         // Send an SMS.
-        smsClient.send(config.sms.toNumber, error.message || error);
-
-        return;
+        return smsClient.send(config.sms.toNumber, error.message || error);
     }
 
     if (!activityOccurred) {
