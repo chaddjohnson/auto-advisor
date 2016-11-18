884e6087c9a4432e891d354d5def6627c025923d
Chad Johnson <chad.d.johnson@gmail.com>
2016-11-18 11:37:13 -0600

    Streaming now restarts when the connection dies, and data is recorded to the database when using the TradeKing client for streaming.

--- src/micro/stream.js
+++ src/micro/stream.js
@@ -7,24 +7,31 @@ var config = require('../../config');
 var mongoose = require('mongoose');
 var OAuth = require('oauth').OAuth;
 var _ = require('lodash');
-var colors = require('colors');
 var Quote = require('./quoteModel');
 
 // Settings
 var symbols = ['AMZN','AAPL','FB','MSFT','QQQ'];
 
-// Set up the trading client.
-var tradingClient = require('../../lib/tradingClients/base').factory('tradeking', config.brokerage);
+mongoose.connect('mongodb://localhost/trading');
+mongoose.connection.on('error', console.error.bind(console, 'Database connection error:'));
 
-// Stream data.
-var stream = tradingClient.stream(symbols);
+startStreaming();
 
-stream.on('data', function(data) {
-    console.log(data);
-});
-stream.on('error', function(error) {
-    console.error(error);
-});
-stream.on('close', function() {
-    console.log('connection closed');
-});
+function startStreaming() {
+    // Set up the trading client.
+    var tradingClient = require('../../lib/tradingClients/base').factory('tradeking', config.brokerage);
+
+    // Stream data.
+    var stream = tradingClient.stream(symbols);
+
+    stream.on('rawData', function(data) {
+        console.log(data);
+    });
+    stream.on('data', function(data) {
+        Quote.create(data);
+    });
+    stream.on('close', function() {
+        // Restart streaming.
+        startStreaming();
+    });
+}
