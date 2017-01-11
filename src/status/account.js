e9c7fa71bda0751e588decf7353fa138e9d5cbed
Chad Johnson <chad.d.johnson@gmail.com>
2017-01-10 23:30:25 -0600

    Fixed broken account status script.

--- src/status/account.js
+++ src/status/account.js
@@ -1,4 +1,5 @@
 var config = require('../../config.json');
+var _ = require('lodash');
 var colors = require('colors');
 var tradingClient = require('../../lib/tradingClients/base').factory('tradeking', config.brokerage);
 
@@ -7,19 +8,29 @@ function formatDollars(number) {
 }
 
 tradingClient.getAccount().then(function(accountData) {
-    var gainLoss = accountData.marketValue - accountData.holdingCostBasis;
-    var gainLossPercentage = (gainLoss / accountData.holdingCostBasis) * 100;
+    tradingClient.getHoldings().then(function(holdingsData) {
+        var marketValue = 0;
+        var costBasis = 0;
 
-    console.log('Value:\t\t' + formatDollars(accountData.value));
-    console.log('Investment:\t' + formatDollars(accountData.holdingCostBasis));
+        _.each(holdingsData, function(holding) {
+            marketValue += holding.marketValue;
+            costBasis += holding.costBasis;
+        });
 
-    if (gainLoss > 0) {
-        console.log('Gain/loss:\t' + colors.green.bold(formatDollars(gainLoss) + ' (' + gainLossPercentage.toFixed(2) + '%)'));
-    }
-    else if (gainLoss < 0) {
-        console.log('Gain/loss:\t' + colors.red.bold(formatDollars(gainLoss) + ' (' + gainLossPercentage.toFixed(2) + '%)'));
-    }
-    else {
-        console.log('Gain/loss:\t' + colors.bold(formatDollars(gainLoss) + ' (' + gainLossPercentage.toFixed(2) + '%)'));
-    }
+        var gainLoss = marketValue - costBasis;
+        var gainLossPercentage = (gainLoss / costBasis) * 100;
+
+        console.log('Value:\t\t' + formatDollars(accountData.value));
+        console.log('Investment:\t' + formatDollars(costBasis));
+
+        if (gainLoss > 0) {
+            console.log('Gain/loss:\t' + colors.green.bold(formatDollars(gainLoss) + ' (' + gainLossPercentage.toFixed(2) + '%)'));
+        }
+        else if (gainLoss < 0) {
+            console.log('Gain/loss:\t' + colors.red.bold(formatDollars(gainLoss) + ' (' + gainLossPercentage.toFixed(2) + '%)'));
+        }
+        else {
+            console.log('Gain/loss:\t' + colors.bold(formatDollars(gainLoss) + ' (' + gainLossPercentage.toFixed(2) + '%)'));
+        }
+    });
 });
