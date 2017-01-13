38eb969cb28c81fd9eba73351bfb15af0e60265a
Chad Johnson <chad.d.johnson@gmail.com>
2017-01-12 23:53:03 -0600

    Fixed testWithMargin.js.

--- src/optimization/testWithMargin.js
+++ src/optimization/testWithMargin.js
@@ -15,18 +15,17 @@ var positions = [];
 var data = require('../../data/' + symbol + '.json');
 
 // Settings
-var phenotype = {"investmentDivisor":3.14365,"sellTriggerProfitPercentage":2.66697,"stopLossThreshold":5.58246,"recentLargeChangeCounterStart":3,"minPercentChangeBuy":-3.85875,"maxPercentChangeBuy":6.55289};
+var phenotype = {"investmentDivisor":5.83481,"sellTriggerProfitPercentage":1.31727,"stopLossThreshold":8.7725};
+var pullOutDates = ["2016-10-27","2016-07-28","2016-04-28","2016-01-28","2015-10-22","2015-07-23","2015-04-23","2015-01-29","2014-10-23","2014-07-24","2014-04-24","2014-01-30","2013-10-24","2013-07-25","2013-04-25","2013-01-29","2012-10-25","2012-07-26","2012-04-26","2012-01-31"];
+
 var balance = 100000 * 2;
 var startingBalance = balance;
 var balanceBeforeBuy = balance / 2;
 var commission = 4.95;
 var baseInvestment = startingBalance / phenotype.investmentDivisor;
 var firstBuyDate = 0;
-var longHoldCount = 0;
-var maxLongHoldCount = 100;
 var daysHeld = 0;
 var index = 0;
-var recentLargeChangeCounter = 0;
 var loss = 0;
 var marginUsed = 0;
 var dateMarginUsed = 0;
@@ -58,6 +57,7 @@ data.forEach(function(dataPoint) {
     var targetSellPrice = averagePositionCostBasis * (1 + (phenotype.sellTriggerProfitPercentage / 100));
     var targetSellPriceReached = dataPoint.close >= targetSellPrice;
     var stopLossThresholdReached = dataPoint.close <= averagePositionCostBasis * (1 - (phenotype.stopLossThreshold / 100));
+    var isPullOutDate = pullOutDates.indexOf(dataPoint.date) > -1;
 
     daysHeld = Math.round((new Date(dataPoint.date) - new Date(firstBuyDate)) / 24 / 60 / 60 / 1000);
 
@@ -65,7 +65,7 @@ data.forEach(function(dataPoint) {
         daysHeld = 0;
     }
 
-    if (positions.length && (stopLossThresholdReached || targetSellPriceReached)) {
+    if (positions.length && (stopLossThresholdReached || targetSellPriceReached || isPullOutDate)) {
         let grossProfit = (shareSum * dataPoint.close) - commission;
         let netProfit = grossProfit - costBasisSum;
         let marginDays = 0;
@@ -85,10 +85,6 @@ data.forEach(function(dataPoint) {
         marginUsed = 0;
         dateMarginUsed = 0;
 
-        if (daysHeld > maxLongHoldCount) {
-            longHoldCount++;
-        }
-
         if (netProfit < 0) {
             loss += netProfit * -1;
         }
@@ -96,49 +92,46 @@ data.forEach(function(dataPoint) {
         console.log(symbol + '\t' + 'SELL' + '\t' + dataPoint.date + '\t' + percentChange.toFixed(2) + '\t' + shareSum + '\t$' + dataPoint.close.toFixed(4) + '\t\t\t$' + grossProfit.toFixed(2) + '  \t$' + netProfit.toFixed(2) + '  \t$' + balance.toFixed(2) + ' ($' + (balance / 2).toFixed(2) + ')' + '\t' + daysHeld);
     }
 
-    if (percentChange !== 0 && percentChange > phenotype.minPercentChangeBuy && percentChange < phenotype.maxPercentChangeBuy) {
-        if (recentLargeChangeCounter <= 0) {
-            let position = {};
-            let investment = Math.sqrt(Math.abs(percentChange)) * baseInvestment;
+    if (!isPullOutDate) {
+        let position = {};
+        let investment = Math.sqrt(Math.abs(percentChange)) * baseInvestment;
 
-            position.shares = Math.floor(investment / dataPoint.close);
-            position.pricePerShare = dataPoint.close;
-            position.costBasis = (position.shares * position.pricePerShare) + commission;
+        position.shares = Math.floor(investment / dataPoint.close);
+        position.pricePerShare = dataPoint.close;
+        position.costBasis = (position.shares * position.pricePerShare) + commission;
 
-            // Ensure adding the position will not exceed the balance.
-            if (balance - position.costBasis > 0 && position.shares > 0) {
-                positions.push(position);
+        // Ensure adding the position will not exceed the balance.
+        if (balance - position.costBasis > 0 && position.shares > 0) {
+            positions.push(position);
 
-                balance -= position.costBasis;
+            balance -= position.costBasis;
 
-                if (!firstBuyDate) {
-                    firstBuyDate = dataPoint.date;
-                    daysHeld = 0;
-                }
-
-                if (balance < balanceBeforeBuy) {
-                    if (!dateMarginUsed) {
-                        dateMarginUsed = dataPoint.date;
-                    }
-
-                    marginUsed = Math.abs(balance - balanceBeforeBuy);
-                }
-
-                console.log(symbol + '\t' + 'BUY' + '\t' + dataPoint.date + '\t' + percentChange.toFixed(2) + '\t' + position.shares + '\t$' + position.pricePerShare.toFixed(4) + '\t  $' + position.costBasis.toFixed(2) + '\t\t\t\t\t  $' + balance.toFixed(2));
+            if (!firstBuyDate) {
+                firstBuyDate = dataPoint.date;
+                daysHeld = 0;
             }
+
+            if (balance < balanceBeforeBuy) {
+                if (!dateMarginUsed) {
+                    dateMarginUsed = dataPoint.date;
+                }
+
+                marginUsed = Math.abs(balance - balanceBeforeBuy);
+            }
+
+            console.log(symbol + '\t' + 'BUY' + '\t' + dataPoint.date + '\t' + percentChange.toFixed(2) + '\t' + position.shares + '\t$' + position.pricePerShare.toFixed(4) + '\t  $' + position.costBasis.toFixed(2) + '\t\t\t\t\t  $' + balance.toFixed(2));
         }
-    }
-    else {
-        recentLargeChangeCounter = phenotype.recentLargeChangeCounterStart;
     }
 
     previousPrice = dataPoint.close;
     previousDate = dataPoint.date;
-    recentLargeChangeCounter--;
 });
 
-console.log('\nLong holds: ' + longHoldCount);
-console.log('\nLoss: ' + loss.toFixed(2));
+console.log();
+console.log(JSON.stringify(phenotype));
+// console.log('Account Value: ' + accountValue.toFixed(2));
+// console.log('Profit: ' + (accountValue - startingBalance).toFixed(2));
+// console.log('Loss: ' + loss.toFixed(2));
 
 function getMarginInterestRate(amount) {
     if (amount < 5000) {
